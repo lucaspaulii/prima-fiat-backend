f1de75582824995bfa0087a121f3483619d6b12d
lucaspaulii <lucaspauli22@gmail.com>
2023-05-16 15:57:48 -0300

    fix: start script

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,6 @@
 import express from "express";
 import cors from "cors";
-import dotenv from "./loadEnv.js";
 import orderRouter from "./routers/orderRouter.js";
-
-dotenv();
 
 const server = express();
 server.use(express.json());
